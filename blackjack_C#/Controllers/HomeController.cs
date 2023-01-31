2e9a977a8a37f3e39097da7d892410539efc1259
Yongmin Kim <kim.yongmin248@gmail.com>
2023-01-30 20:51:47 -0600

    MVC update

---
+++ blackjack_C#/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using blackjack_C_.Models;
+
+namespace blackjack_C_.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
